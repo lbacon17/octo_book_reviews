1c91635cad222acc4fc761af15ae906179e067ee
lbacon17 <louis.bacon17@icloud.com>
2021-09-09 21:48:34 +0000

    added django crispy forms

--- octo_book_reviews/settings.py
+++ octo_book_reviews/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
     'home',
     'books',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'octo_book_reviews.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
