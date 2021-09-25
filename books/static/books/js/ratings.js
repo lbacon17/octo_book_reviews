0a9304d06ed126427ff81d318bd9ba773f34e442
lbacon17 <louis.bacon17@icloud.com>
2021-09-25 15:58:03 +0000

    created rating form

--- books/static/books/js/ratings.js
+++ books/static/books/js/ratings.js
@@ -1,58 +1,53 @@
-console.log("hello world");
-
 const oneStar = document.getElementById('oneStar');
 const twoStar = document.getElementById('twoStar');
 const threeStar = document.getElementById('threeStar');
 const fourStar = document.getElementById('fourStar');
 const fiveStar = document.getElementById('fiveStar');
 
+const ratingForm = document.querySelector('.rating-form');
+const confirmBox = document.getElementById('confirm-box');
+const csrf = document.getElementsByName('csrfmiddlewaretoken');
+
+const handleStarSelect = (size) => {
+    const children = ratingForm.children;
+    for (let i=0; i < children.length; i++) {
+        if (i <= size) {
+            children[i].classList.add('checked');
+        } else {
+            children[i].classList.remove('checked');
+        }
+    }
+}
+
 const handleSelect = (selection) => {
     switch(selection){
         case 'oneStar': {
-            oneStar.classList.add('checked');
-            twoStar.classList.remove('checked');
-            threeStar.classList.remove('checked');
-            fourStar.classList.remove('checked');
-            fiveStar.classList.remove('checked');
+            handleStarSelect(1);
             return
         }
         case 'twoStar': {
-            oneStar.classList.add('checked');
-            twoStar.classList.add('checked');
-            threeStar.classList.remove('checked');
-            fourStar.classList.remove('checked');
-            fiveStar.classList.remove('checked');
+            handleStarSelect(2);
             return
         }
         case 'threeStar': {
-            oneStar.classList.add('checked');
-            twoStar.classList.add('checked');
-            threeStar.classList.add('checked');
-            fourStar.classList.remove('checked');
-            fiveStar.classList.remove('checked');
+            handleStarSelect(3);
             return
         }
         case 'fourStar': {
-            oneStar.classList.add('checked');
-            twoStar.classList.add('checked');
-            threeStar.classList.add('checked');
-            fourStar.classList.add('checked');
-            fiveStar.classList.remove('checked');
+            handleStarSelect(4);
             return
         }
         case 'fiveStar': {
-            oneStar.classList.add('checked');
-            twoStar.classList.add('checked');
-            threeStar.classList.add('checked');
-            fourStar.classList.add('checked');
-            fiveStar.classList.add('checked');
+            handleStarSelect(5);
             return
         }
     }
 }
 
-const ratings = [oneStar, twoStar, threeStar, fourStar, fiveStar];
-
-ratings.forEach(item => item.addEventListener('mouseover', (event)=>{
-    handleSelect(event.target.id)
-}))
+if(oneStar) {
+    const ratings = [oneStar, twoStar, threeStar, fourStar, fiveStar];
+    
+    ratings.forEach(item => item.addEventListener('mouseover', (event)=>{
+        handleSelect(event.target.id)
+    }))
+}
